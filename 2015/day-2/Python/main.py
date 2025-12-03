92b93729654b93256d9c042c07a3a9750ea93e32
daniel-egan <95421705+daniel-egan@users.noreply.github.com>
2025-12-03 12:51:23 +0000

    Completed part 2

--- 2015/day-2/Python/main.py
+++ 2015/day-2/Python/main.py
@@ -1,25 +1,54 @@
 file = open("day2.txt", "r")
 
 totalAreaRequired = 0
+totalRibbon = 0
 
 for line in file:
+    # line = "2x3x4"
+    # line = "1x1x10"
     # 2*l*w + 2*w*h + 2*h*l    
-    lineArray = line.split("x")
+    lineArray = line.strip().split("x")
+    print(lineArray)
 
     length = int(lineArray[0])
     width = int(lineArray[1])
     height = int(lineArray[2])
 
-    primaryArea = (length*width)
-    secondaryArea = (width*height)
-    tertiaryArea = (length*height)
+    # primaryArea = (length*width)
+    # secondaryArea = (width*height)
+    # tertiaryArea = (length*height)
 
-    boxSurfaceArea = 2*primaryArea + 2*secondaryArea + 2*tertiaryArea
+    # boxSurfaceArea = 2*primaryArea + 2*secondaryArea + 2*tertiaryArea
 
-    minArea = min(primaryArea, secondaryArea, tertiaryArea)
+    # minArea = min(primaryArea, secondaryArea, tertiaryArea)
 
-    finalAreaRequied = boxSurfaceArea + minArea
+    # finalAreaRequied = boxSurfaceArea + minArea
     
-    totalAreaRequired += finalAreaRequied
+    # totalAreaRequired += finalAreaRequied
 
-print(totalAreaRequired)
+    ############## Part 2
+    
+    sortedArray = sorted([length, width, height])
+    print(sortedArray)
+
+    smallestDimension = sortedArray[0]
+    secondSmallestDimension = sortedArray[1]
+
+    print("smallestDimension", smallestDimension, "secondSmallestDimension", secondSmallestDimension)
+
+    wrappingRibbon = int(smallestDimension) + int(smallestDimension) + int(secondSmallestDimension) + int(secondSmallestDimension)
+
+    print("wrappingRibbon", wrappingRibbon)
+
+    cubicVolume = length * width * height
+
+    print("cubicVolume", cubicVolume)
+
+    ribbonRequired = wrappingRibbon + cubicVolume
+
+    totalRibbon += ribbonRequired
+
+    print("totalRibbon", totalRibbon)
+
+# print(totalAreaRequired)
+print(totalRibbon)

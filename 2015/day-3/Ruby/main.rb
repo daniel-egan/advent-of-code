ef9f6d8e83e1473374b0b2acae51a1f912d46097
daniel-egan <95421705+daniel-egan@users.noreply.github.com>
2025-12-03 23:14:55 +0000

    Completed Part 2

--- 2015/day-3/Ruby/main.rb
+++ 2015/day-3/Ruby/main.rb
@@ -1,7 +1,17 @@
-file = File.read("temp.txt")
+file = File.read("day3.txt")
 
-currentX = 0
-currentY = 0
+class Santa
+  attr_accessor :currentX, :currentY
+
+  def initialize
+    @currentX = 0
+    @currentY = 0
+  end
+end
+
+santa = Santa.new
+roboSanta = Santa.new
+currentlyNormalSanta = true
 
 housesHashMap = Hash.new(0)
 housesHashMap[[0,0]] += 1
@@ -9,20 +19,39 @@ housesHashMap[[0,0]] += 1
 splitArray = file.split(//)
 
 for direction in splitArray do
+    case currentlyNormalSanta
+    when true
+        case direction
+        when "^"
+            santa.currentY += 1
+            housesHashMap[[santa.currentX, santa.currentY]] += 1
+        when "<"
+            santa.currentX -= 1
+            housesHashMap[[santa.currentX, santa.currentY]] += 1
+        when "v"
+            santa.currentY -= 1
+            housesHashMap[[santa.currentX, santa.currentY]] += 1
+        when ">"
+            santa.currentX += 1
+            housesHashMap[[santa.currentX, santa.currentY]] += 1
+        end
+    when false
     case direction
-    when "^"
-        currentY += 1
-        housesHashMap[[currentX, currentY]] += 1
-    when "<"
-        currentX -= 1
-        housesHashMap[[currentX, currentY]] += 1
-    when "v"
-        currentY -= 1
-        housesHashMap[[currentX, currentY]] += 1
-    when ">"
-        currentX += 1
-        housesHashMap[[currentX, currentY]] += 1
+        when "^"
+            roboSanta.currentY += 1
+            housesHashMap[[roboSanta.currentX, roboSanta.currentY]] += 1
+        when "<"
+            roboSanta.currentX -= 1
+            housesHashMap[[roboSanta.currentX, roboSanta.currentY]] += 1
+        when "v"
+            roboSanta.currentY -= 1
+            housesHashMap[[roboSanta.currentX, roboSanta.currentY]] += 1
+        when ">"
+            roboSanta.currentX += 1
+            housesHashMap[[roboSanta.currentX, roboSanta.currentY]] += 1
+        end
     end
+    currentlyNormalSanta = !currentlyNormalSanta
 end
 
 puts housesHashMap.length

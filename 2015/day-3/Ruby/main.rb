56b60e669ae872b010861d13be24f7a6689d4f5c
daniel-egan <95421705+daniel-egan@users.noreply.github.com>
2025-12-03 23:01:01 +0000

    Part 1 completed

--- 2015/day-3/Ruby/main.rb
+++ 2015/day-3/Ruby/main.rb
@@ -1,30 +1,28 @@
 file = File.read("temp.txt")
 
-numberOfHouses = 1
+currentX = 0
+currentY = 0
 
-x = 0
-y = 0
-
-housesHashMap = {}
+housesHashMap = Hash.new(0)
+housesHashMap[[0,0]] += 1
 
 splitArray = file.split(//)
 
 for direction in splitArray do
     case direction
     when "^"
-        puts "UP"
-        y += 1
-        housesHashMap[:(x,y)] += 1
+        currentY += 1
+        housesHashMap[[currentX, currentY]] += 1
     when "<"
-        puts "LEFT"
-        x -= 1
+        currentX -= 1
+        housesHashMap[[currentX, currentY]] += 1
     when "v"
-        puts "DOWN"
-        y -= 1
+        currentY -= 1
+        housesHashMap[[currentX, currentY]] += 1
     when ">"
-        puts "RIGHT"
-        x += 1
+        currentX += 1
+        housesHashMap[[currentX, currentY]] += 1
     end
 end
 
-puts housesHashMap
+puts housesHashMap.length

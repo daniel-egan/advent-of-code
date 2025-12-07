c182a938e6fabf966d0da51503abc1d82edaf7a0
daniel-egan <95421705+daniel-egan@users.noreply.github.com>
2025-12-07 20:35:06 +0000

    Added file reading

--- 2015/day-6/Perl/main.pl
+++ 2015/day-6/Perl/main.pl
@@ -1,1 +1,15 @@
-print("Hello World\n");
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $file = "temp.txt";
+
+my $niceCount = 0;
+
+open my $fh, '<', $file or die "Could not open file '$file': $!";
+
+while (my $line = <$fh>) {
+    chomp $line;
+    print "$line\n";
+
+}

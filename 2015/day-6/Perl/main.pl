f57f104a9cc931362dc8a383c5b508e65b1743e8
daniel-egan <95421705+daniel-egan@users.noreply.github.com>
2025-12-09 18:03:15 +0000

    Continuing part 1

--- 2015/day-6/Perl/main.pl
+++ 2015/day-6/Perl/main.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/perl
+
 use strict;
 use warnings;
 
@@ -11,51 +12,58 @@ open my $fh, '<', $file or die "Could not open file '$file': $!";
 # 0 means lights are off
 # 1 means lights are on
 my %lights = (
-    [0,0] => 0,
-    [999,999] => 0
+  '0,0'     => 0,
+  '999,999' => 0
 );
 
 my $currentLightCount = 0;
 
 while (my $line = <$fh>) {
-    chomp $line;
-    my @lineArray = split ' ', $line;
+  chomp $line;
+  my @lineArray = split ' ', $line;
 
-    my firstCoords = @lineArray[-3];
-    my endCoords = @lineArray[-1];
+  my @firstCoords = @lineArray[-3];
+  my @endCoords   = @lineArray[-1];
 
-    # if ($line =~ m/toggle/){
-    #     print "TOGGLE\n";
-    #     my $currentLight = $lights{[0,0]};
+  for (my $coordElement = 0; $coordElement < 2; $coordElement++) {
+    print "hi $coordElement\n";
+    print @firstCoords[$coordElement];
+  }
 
-    #     if ($currentLight == 0){
-    #         turnLightOn([0,0]);
-    #     } elsif ($currentLight == 1){
-    #         turnLightOff([0,0]);
-    #     } else {
-    #         turnLightOn([0,0]);
-    #     }
-    # } elsif ($line =~ m/turn on/){
-    #     print "TURN ON\n";
-    # } elsif ($line =~ m/turn off/){
-    #     print "TURN OFF\n";
-    # } else{
-    #     print "RegEx Error";
-    #     exit
-    # }
+  if ($line =~ /toggle/) {
+    print "TOGGLE\n";
+    my $currentLight = $lights{'0,0'};
+
+    if ($currentLight == 0) {
+      # turnLightOn('0,0');
+    }
+    elsif ($currentLight == 1) {
+      # turnLightOff('0,0');
+    } else {
+      # turnLightOn('0,0');
+    }
+  }
+  elsif ($line =~ /turn on/) {
+    # print "TURN ON\n";
+  }
+  elsif ($line =~ /turn off/) {
+    # print "TURN OFF\n";
+  } else {
+    # print "RegEx Error";
+    # exit
+  }
 }
 
 sub turnLightOn {
-    my $lightKey = $_[0];
+  my $lightKey = $_[0];
 
-    $currentLightCount += 1;
+  $currentLightCount++;
 }
 
 sub turnLightOff {
-    my $lightKey = $_[0];
+  my $lightKey = $_[0];
 
-    $currentLightCount -= 1;
+  $currentLightCount--;
 }
-
 
 print "Current lights on: $currentLightCount\n";

ef9928f03552402b0fde3b19f9e21af91be6f3b1
daniel-egan <95421705+daniel-egan@users.noreply.github.com>
2025-12-09 15:54:32 +0000

    Started progress towards part 1

--- 2015/day-6/Perl/main.pl
+++ 2015/day-6/Perl/main.pl
@@ -8,8 +8,54 @@ my $niceCount = 0;
 
 open my $fh, '<', $file or die "Could not open file '$file': $!";
 
+# 0 means lights are off
+# 1 means lights are on
+my %lights = (
+    [0,0] => 0,
+    [999,999] => 0
+);
+
+my $currentLightCount = 0;
+
 while (my $line = <$fh>) {
     chomp $line;
-    print "$line\n";
+    my @lineArray = split ' ', $line;
 
+    my firstCoords = @lineArray[-3];
+    my endCoords = @lineArray[-1];
+
+    # if ($line =~ m/toggle/){
+    #     print "TOGGLE\n";
+    #     my $currentLight = $lights{[0,0]};
+
+    #     if ($currentLight == 0){
+    #         turnLightOn([0,0]);
+    #     } elsif ($currentLight == 1){
+    #         turnLightOff([0,0]);
+    #     } else {
+    #         turnLightOn([0,0]);
+    #     }
+    # } elsif ($line =~ m/turn on/){
+    #     print "TURN ON\n";
+    # } elsif ($line =~ m/turn off/){
+    #     print "TURN OFF\n";
+    # } else{
+    #     print "RegEx Error";
+    #     exit
+    # }
 }
+
+sub turnLightOn {
+    my $lightKey = $_[0];
+
+    $currentLightCount += 1;
+}
+
+sub turnLightOff {
+    my $lightKey = $_[0];
+
+    $currentLightCount -= 1;
+}
+
+
+print "Current lights on: $currentLightCount\n";
